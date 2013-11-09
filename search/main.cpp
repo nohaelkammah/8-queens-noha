5f8f26487844cd115eca6caea7e8cb9a86f3786d
Noha Mohamed <Nohak@noha.local>
2013-11-09 17:34:11 +0200

    Initial Commit

---
+++ search/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  search
+//
+//  Created by Noha Mohamed on 11/9/13.
+//  Copyright (c) 2013 Noha Mohamed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
