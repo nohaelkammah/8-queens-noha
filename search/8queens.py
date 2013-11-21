4d76bc4d3e3c68b1bd2e3974b92db347fdb9b193
nohaelkammah <nohaelkammah@hotmail.com>
2013-11-22 01:08:23 +0200

    cleaned up. New condition there cant be two queens in the same column

--- search/8queens.py
+++ search/8queens.py
@@ -1,27 +1,27 @@
 import random
-
-
-
 finished = 0
 #place queens
 
 while finished == 0:
+    col = [0,1,2,3,4,5,6,7]
+    random.shuffle(col)
+
     board = [[0 for x in xrange(8)] for x in xrange(8)] #matrix
     for i in range (0,8,1):
         r = random.randrange(0,7)
-        c = random.randrange(0,7)
-        board[r][c]= 'Q'
+        board[r][col[i]]= 'Q'
         
-    count = 0
+    count = 0 # count the queens in case there are repetitions
 
     for c in range (0,8,1):
         for r in range (0,8,1):
             if board[r][c]== 'Q':
                 count = count +1
-    print count
     if count == 8:
         finished = 1
     else:
         continue
 
+
 print board
+
